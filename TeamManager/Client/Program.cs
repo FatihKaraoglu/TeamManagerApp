839fd8653320bdb8b234a16e317ef60b58719f5c
FatihKaraoglu <karaoglu.fatih16@gmail.com>
2024-03-25 13:52:33 +0100

    Init Create
    Inital Commit

---
+++ TeamManager/Client/Program.cs
@@ -0,0 +1,24 @@
+global using TeamManager.Shared;
+global using System.Net.Http.Json;
+global using TeamManager.Client.Services.AuthService;
+global using Microsoft.AspNetCore.Components.Authorization;
+using TeamManager.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
+using MudBlazor.Services;
+using MudBlazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMudServices();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+await builder.Build().RunAsync();
---
+++ TeamManager/Client/Shared/MixedTheme.cs
@@ -0,0 +1,23 @@
+﻿using MudBlazor;
+using System.Drawing;
+
+namespace TeamManager.Client.Shared
+{
+    public class MixedTheme : MudTheme
+    {
+        public Palette Palette => new Palette
+        {
+            Primary = "#8bc5bb", // Set your primary color
+            Secondary = Colors.Red.Default, // Set your secondary color
+            Background = "#222a28", // Set background color
+            Surface = "#373e3c;", // Set surface color
+            AppbarBackground = "#4d5352", // Set appbar background color
+            DrawerBackground = "#4d5352"
+        };
+
+        public LayoutProperties LayoutProperties => new LayoutProperties
+        {
+            DrawerWidthLeft = "240px"
+        };
+    }
+}
